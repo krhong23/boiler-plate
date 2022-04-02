c595f3455624857cc3fdbf8f37fb8f97558d5a5d
rami <krhong23@gmail.com>
2022-04-02 23:49:13 +0900

    설정정보 관리 - 원격레파지토리에서 dev.js 삭제

--- config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://rami:rami!2345@cluster0-shard-00-00.ioixk.mongodb.net:27017,cluster0-shard-00-01.ioixk.mongodb.net:27017,cluster0-shard-00-02.ioixk.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-47q39o-shard-0&authSource=admin&retryWrites=true&w=majority'
-}
