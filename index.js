31d2ccb5bac5be1bc27a6abb6e00ca36714a2344
rami <krhong23@gmail.com>
2022-04-03 00:51:09 +0900

    로그인 기능 구현 - POST /login - 요청된 email이 DB에 있는지 확인 - DB에 저장된 password(암호화O)와 요청된 plain password(암호화X) 비교 > comparePassword - jsonwebtoken 설치 - 비밀번호가 일치시 토큰 생성(jwt) - cookie-parser 설치 - 토큰 cookie에 저장

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5001
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const config = require("./config/key")
 const {User} = require("./models/User")
 
@@ -10,6 +11,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // application/json
 app.use(bodyParser.json());
+
+app.use(cookieParser());
 
 // Mongo DB Connection
 const mongoose = require('mongoose')
@@ -30,6 +33,36 @@ app.post('/register', (req, res) => {
         return res.status(200).json({
             success: true
         })
+    })
+})
+
+app.post('/login', (req, res) => {
+    // 요청된 email을 데이터베이스에서 찾는다.
+    User.findOne({email: req.body.email}, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 사용자가 없습니다."
+            })
+        }
+
+        // 요청된 email이 데이터베이스에 있으면 비밀번호가 맞는지 확인한다.
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({
+                    loginSuccess: false,
+                    message: "비밀번호가 틀렸습니다."
+                })
+            // 비밀번호가 맞을 경우 토큰을 생성한다.
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+                // 토큰을 저장한다. where?  cookie or local storage, ..
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({loginSuccess: true, message: user._id})
+            })
+        })
+
     })
 })
 app.listen(port, () => {
