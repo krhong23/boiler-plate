136d76dc48503a167c057952379a530057f3646b
rami <krhong23@gmail.com>
2022-04-12 23:43:58 +0900

    로그인 페이지 구현 - add Antd CSS framework - redux, react-redux, redux middleware(redux-promise, redux-thunk) - action, reducer, page

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,15 +1,59 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
+import {useDispatch} from "react-redux";
+import {loginUser} from "../../../actions/user_action";
+import { useNavigate } from 'react-router-dom';
 
-class LoginPage extends Component {
-    render() {
-        return (
-            <div>
-                Login Page
-            </div>
-        );
+function LoginPage() {
+
+    const dispatch = useDispatch();
+const navigate = useNavigate();
+    // define state
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
     }
-}
 
-LoginPage.propTypes = {};
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onsubmitHandler = (event) => {
+        // page가 refresh 되는 것을 방지
+        event.preventDefault();
+
+        let body = {
+            email: Email,
+            password: Password
+        }
+
+        dispatch(loginUser(body))
+            .then(response => {
+                if (response.payload.loginSuccess) {
+                    navigate('/');
+                } else {
+                    alert('Error')
+                }
+            })
+    }
+
+    return (
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'}}>
+            <form style={{display: 'flex', flexDirection: 'column'}}
+                  onSubmit={onsubmitHandler}
+            >
+                <label>Email</label>
+                <input type='email' value={Email} onChange={onEmailHandler}/>
+                <label>Password</label>
+                <input type='password' value={Password} onChange={onPasswordHandler}/>
+                <br/>
+                <button>
+                    Login
+                </button>
+            </form>
+        </div>
+    );
+}
 
 export default LoginPage;
